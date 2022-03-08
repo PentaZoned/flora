4596f78f3a4909864dd1fc98fb62f6b6494955b7
Baofeng Guo <78279120+magickw@users.noreply.github.com>
2022-03-07 19:11:31 -0800

    completed the preloader.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,44 @@
+import React, { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
+import Preloader from "../src/components/Preloader";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+//website components
+import Navbar from "./components/Navbar";
+import Home from "./components/Home/Home";
+//put other website components here
+
+import Footer from "./components/Footer";
 
 function App() {
+ 
+    // preloader
+    const [load, upadateLoad] = useState(true);
+  
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
+    
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <Router>
+  <Preloader load={load} />
+  <div className="App" id={load ? "no-scroll" : "scroll"}>
+    <Navbar />
+    <ScrollToTop />
+    <Switch>
+      <Route path="/" exact component={Home} />
+       {/* put the navbar items here*/}
+
+    </Switch>
+    <Footer />
+  </div>
+</Router>
+);
 }
 
 export default App;
