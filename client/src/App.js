500861d4358e4a4c8df1da125876218da08f2d5e
Baofeng Guo <78279120+magickw@users.noreply.github.com>
2022-03-07 19:51:14 -0800

    added css code for preloader

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
 import Preloader from "../src/components/Preloader";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 //website components
 import Navbar from "./components/Navbar";
-import Home from "./components/Home/Home";
+import Home from "./components/pages/Home";
 //put other website components here
 
 import Footer from "./components/Footer";
@@ -19,7 +19,7 @@ function App() {
   useEffect(() => {
     const timer = setTimeout(() => {
       upadateLoad(false);
-    }, 1200);
+    }, 2000);
     
     return () => clearTimeout(timer);
   }, []);
@@ -29,12 +29,11 @@ function App() {
   <Preloader load={load} />
   <div className="App" id={load ? "no-scroll" : "scroll"}>
     <Navbar />
-    <ScrollToTop />
-    <Switch>
+    <Routes>
       <Route path="/" exact component={Home} />
        {/* put the navbar items here*/}
 
-    </Switch>
+    </Routes>
     <Footer />
   </div>
 </Router>
