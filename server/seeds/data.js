e1785694f2cf8c71f23020124d71879b17b869a7
Scott Everett <burningcupidxxx@gmail.com>
2022-03-10 12:06:09 -0800

    Add development data to seed.js

--- server/seeds/data.js
+++ server/seeds/data.js
@@ -1,19 +1,58 @@
 const products = [
     {
-        title: "Tuberose Boquet",
-        description: "A lovely boquet of tuberoses complimented by eucalyptus leaves.",
+        title: "Glorius Gladioli",
+        description: "A lovely boquet of Stephens favorite.",
+        category: 'Gladioli',
+        price: 12.99,
         likes: "112",
     },
     {
-        title: "White Roses",
-        description: "A lovely boquet of white roses.",
-        likes: "96",
+        title: "Spring is the Cruelest Season",
+        description: "Smart arrangements of mixed selections of complimentary flowers.",
+        category: 'Mixed Bouqets',
+        price: 12.99,
+        likes: "112",
     },
     {
-        title: "Carnations",
-        description: "A lovely boquet of mixed carnations.",
-        likes: "76",
+        title: "The Kansas Special",
+        description: "Ten cheerful, golden sunflowers that instantly brighten any room.",
+        category: 'Sunflowers',
+        price: 12.99,
+        likes: "112",
     },
-]
+];
 
-module.exports = products;
+const users = [
+    {
+        firstName: 'Leo',
+        lastName: 'Lionni',
+        email: 'frederick@aol.com',
+        password: '4fieldMice',
+    },
+    {
+        firstName: 'Fyodor',
+        lastName: 'Dostoevski',
+        email: 'theidot@hotmail.com',
+        password: 'AmICancelled?',
+    },
+    {
+        firstName: 'Rocket',
+        lastName: 'Power',
+        email: 'oceanshoresrealty@gmail.com',
+        password: 'NoShoobies!'
+    }
+];
+
+const categories = [
+    {
+        name: 'Gladioli',
+    },
+    {
+        name: 'Mixed Bouqets',
+    },
+    {
+        name: 'Sunflowers',
+    }
+];
+
+module.exports = { products, users, categories };
--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,15 +1,21 @@
 const connection = require('../config/connection');
-const { User, Product } = require('../models');
-const products = require('./data');
+const { User, Product, Category } = require('../models');
+const { users, products, categories } = require('./data');
 
 connection.once('open', async () => {
     console.log('connected');
 
+    await User.deleteMany({});
     await Product.deleteMany({});
+    await Category.deleteMany({});
 
+    await User.collection.insertMany(users);
     await Product.collection.insertMany(products);
+    await Category.collection.insertMany(categories);
 
+    console.table(users);
     console.table(products);
+    console.table(categories);
     console.info('Seeding complete!');
     process.exit(0);
 });
