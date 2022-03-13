c23940857ad6c4de15aa262a7dfcec84978c70bb
Mandy Tsang <mandy.tsang007@gmail.com>
2022-03-12 17:17:40 -0800

    add query hooks in detail.js

--- client/src/components/pages/Detail.js
+++ client/src/components/pages/Detail.js
@@ -1,8 +1,66 @@
-import React, {useEffect, useState} from 'react';
+import React, {Profiler, useEffect, useState} from 'react';
 import { Link, useParams } from 'react-router-dom';
-import {useQuery} from '@apollo/client';
+import {useLazyQuery, useQuery} from '@apollo/client';
 
-import Card from 'client/src/components/pages/Cart.js';
+import Cart from 'client/src/components/pages/Cart.js';
 import {useStoreContext} from 'client/src/GlobalState.js';
 import {REMOVE_FROM_CART, UPDATE_CARD_QUANTITY, ADD_TO_CART, UPDATE_PRODUCTS,} from "client/src/utils/actions";
 import {QUERY_PRODUCTS} from '../utils/queries';
+
+function Detail(){
+    const [state, dispatch] = useStoreContext();
+    const {id} = useParams();
+    const [currentProduct, setCurrentProduct] = useState({});
+    const {loading, data} = useQuery(QUERY_PRODUCTS);
+    const {products, cart} =state;
+    
+    useEffect(()=>{
+        if(products.length){
+            setCurrentProduct(products.find((product) => product._id ===id));
+        }
+        else if (data) {
+            dispatch({
+                type: UPDATE_PRODUCTS,
+                products: data.products,
+            });
+        }
+    })
+    
+    const addToCart = () => {
+        const itemInCart = cart.find((cartItem) => cartItem._id === id);
+        if (itemInCart){
+            dispatch({
+                type: UPDATE_CARD_QUANTITY,
+                _id: id,
+                purchaseQuantity: parseInt(itemInCart.purchaseQuantity) +1,
+            })
+        }else {
+            dispatch({
+                type: ADD_TO_CART,
+                product: { ...currentProduct, purchaseQuantity: 1},
+            });
+        }
+    };
+    
+    const removeFromCart =() => {
+        dispatch({
+            type: REMOVE_FROM_CART,
+            _id: currentProduct._id,
+        });
+    }
+    return (
+        <main>
+            <div>
+                {products && products.map((product) => (
+                    <div key={product._id}>
+                        <div> {product.img}</div>
+                        <div> {product.title}</div>
+                        <div> {product.description}</div>
+                        <div> {product.price}</div>  
+                        
+                    </div>
+                ))}
+            </div>
+        </main>
+    )
+}
