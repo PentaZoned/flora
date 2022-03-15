0ae0778afc0ee0b4e791504572e66472c6b01f6f
Baofeng Guo <78279120+magickw@users.noreply.github.com>
2022-03-14 21:27:31 -0700

    updated the navbar

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -7,102 +7,93 @@ function Nav() {
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
-        <div className='d-flex justify-content-between align-items-center custom-nav'>
         <div id='nav-menu'>
-            <Link 
-            to={`/profile`}
-            >
-              
-              <div className='d-flex'>
+            <div className="d-flex">
+              <Link
+                to={`/profile`}
+              >
                 <img className='nav-icon' src='images/account-icon.png' alt='Account Icon'></img>
                 <h6 className='custom-nav-link'> Account</h6>
-              </div>
-            </Link>
-
-            <Link 
-            to={`/cart`}
-            >
-              <div className='d-flex mx-5'>
-                <img className='nav-icon' src='images/cart-icon.png' alt='Account Icon'></img>
-                
-                <h6 className='custom-nav-link'> Cart</h6>
-          
-              </div>
               </Link>
+            </div>
 
-              <Link 
-            to={`/logout`}
-            >
-              <div className='d-flex'>
+            <div className="d-flex mx-5">
+              <Link
+                to={`/cart`}
+              >
+                <img className='nav-icon' src='images/cart-icon.png' alt='Account Icon'></img>
+                <h6 className='custom-nav-link'> Cart</h6>
+              </Link>
+            </div>
+
+            <div className="d-flex">
+              <Link
+                to={`/logout`}
+              >
                 <img className='nav-icon' src='images/logout-icon.png' alt='Account Icon'></img>
                 <h6 className='custom-nav-link' href="/" onClick={() => Auth.logout()}> Logout</h6>
-              </div>
-            </Link>
-        </div>
+              </Link>
+            </div>
         </div>
       );
     } else {
-      return (<div id='nav-menu'>
-      <Link 
-        to={`/cart`}
-        >
-          <div className='d-flex mx-5'>
-            <img className='nav-icon' src='images/cart-icon.png' alt='Account Icon'></img>
-            
-            <h6 className='custom-nav-link'> Cart</h6>
-      
-          </div>
-          </Link>
+      return (
+        <div id='nav-menu'>
+            <div className="d-flex">
+              <Link
+                to={`/cart`}
+              >
+                <img className='nav-icon' src='images/cart-icon.png' alt='Account Icon'></img>
+                <h6 className='custom-nav-link'> Cart</h6>
+              </Link>
+            </div>
 
-        <Link 
-        to={`/login`}
-        >
-          
-          <div className='d-flex'>
-            <img className='nav-icon' src='images/login-icon.png' alt='Account Icon'></img>
-            <h6 className='custom-nav-link'> Login</h6>
-          </div>
-        </Link>
+            <div className="d-flex mx-5">
+              <Link
+                to={`/login`}
+              >
+                <img className='nav-icon' src='images/login-icon.png' alt='Account Icon'></img>
+                <h6 className='custom-nav-link'> Login</h6>
+              </Link>
+            </div>
 
-        
-
-          <Link 
-        to={`/signup`}
-        >
-          <div className='d-flex'>
-            <img className='nav-icon' src='images/signup-icon.png' alt='Account Icon'></img>
-            <h6 className='custom-nav-link'> Sign Up</h6>
-          </div>
-        </Link>
-      </div>
+            <div className="d-flex">
+              <Link
+                to={`/signup`}
+              >
+                <img className='nav-icon' src='images/signup-icon.png' alt='Account Icon'></img>
+                <h6 className='custom-nav-link'> Sign Up</h6>
+              </Link>
+            </div>
+        </div>
       );
     }
   }
-      
 
-    return (
-      <div className='container-fluid px-0'>
-      <div className='d-flex justify-content-between align-items-center custom-nav'>
-      <Link 
-        to={`/`}
+
+  return (
+    <header className='container-fluid px-0'>
+      <div className='flex-row d-flex justify-content-between align-items-center custom-nav'>
+        <Link
+          to={`/`}
         >
-        <div>
           <img src='images/logo.png' alt='Flora Logo' id='logo' width='80px'></img>
-        </div>
         </Link>
         <nav>
-        {showNavigation()}
-      </nav>
+          {showNavigation()}
+        </nav>
+      </div>
 
-        <div className='d-flex align-items-center justify-content-end custom-sub-nav'>
+
+      <div className='d-flex align-items-center justify-content-end custom-sub-nav'>
         <h5 className='menu-item'>Flowers</h5>
         <h5 className='menu-item'>Plants</h5>
         <h5 className='menu-item'>Cards</h5>
         <h5 className='menu-item'>SALE</h5>
       </div>
-    </div>
-    </div>
-    );
+    </header>
+
+  );
 }
 
 export default Nav;
